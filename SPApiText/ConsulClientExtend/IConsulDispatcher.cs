c2299b6a593f531a77f9ffa9abba4cafd0d95ddd
zhengbo <whitezheng@hkolens.com>
2022-04-27 16:10:53 +0800

    提交微服务有关

---
+++ SPApiText/ConsulClientExtend/IConsulDispatcher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SPCoreApiText.ConsulClientExtend
+{
+    public interface IConsulDispatcher
+    {
+        /// <summary>
+        /// 负载均衡获取地址
+        /// </summary>
+        /// <param name="serviceName">朝夕的老师</param>
+        /// <returns></returns>
+        string ChooseAddress(string serviceName);
+    }
+}
---
+++ SPApiText/Utiltiy/HttpHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SPCoreApiText.Utiltiy
+{
+    public class HttpHelper
+    {
+        /// <summary>
+        /// get请求core webapi
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static string InvokeApi(string url)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                HttpRequestMessage message = new HttpRequestMessage();
+                message.Method = HttpMethod.Get;
+                message.RequestUri = new Uri(url);
+                var result = httpClient.SendAsync(message).Result;
+                string content = result.Content.ReadAsStringAsync().Result;
+                return content;
+            }
+        }
+    }
+}
