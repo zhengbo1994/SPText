646f3ebe22e310b8b6e30bd6e69e2f6b6e356f51
郑博 <179720610@qq.com>
2022-07-11 21:02:57 +0800

    提交

---
+++ EF_CodeDB/UserServiceDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EF_CodeDB
+{
+    public partial class CommonServiceDbContext : DbContext
+    {
+        public CommonServiceDbContext(DbContextOptions<CommonServiceDbContext> options) : base(options)
+        {
+            Console.WriteLine($"This is {nameof(CommonServiceDbContext)}  DbContextOptions");
+        }
+
+        public virtual DbSet<Company> Company { get; set; }
+        public virtual DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+    }
+}
---
+++ WebText/Data/CommonServiceDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using WebText.Data.Models;
+
+namespace WebText.Data
+{
+    public partial class CommonServiceDbContext : DbContext
+    {
+        public CommonServiceDbContext(DbContextOptions<CommonServiceDbContext> options) : base(options)
+        {
+            Console.WriteLine($"This is {nameof(CommonServiceDbContext)}  DbContextOptions");
+        }
+
+        public virtual DbSet<Company> Company { get; set; }
+        public virtual DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+    }
+}
