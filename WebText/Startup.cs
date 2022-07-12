566ed7c3f4c2f37d1ca9721d3a998cc0639c453a
zhengbo <whitezheng@hkolens.com>
2022-07-12 13:48:59 +0800

    修改注释

--- WebText/Startup.cs
+++ WebText/Startup.cs
@@ -29,47 +29,49 @@ namespace WebText
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            string conn = this.Configuration.GetConnectionString("UserServiceConnection");
-            string rabbitMQConn = this.Configuration.GetConnectionString("RabbitMQ");
-            services.AddCap(x =>
-            {
-                x.UseSqlServer(conn);//使用SQLServer的容器---数据库连接
-                x.UseRabbitMQ(rabbitMQConn);//使用RabbitMQ---连接地址
-                x.FailedRetryCount = 10;//重试次数
-                x.FailedRetryInterval = 60;//重试的间隔频率
-                x.FailedThresholdCallback = failed =>
-                {
-                    var logger = failed.ServiceProvider.GetService<ILogger<Startup>>();
-                    logger.LogError($@"MessageType {failed.MessageType} 失败了， 重试了 {x.FailedRetryCount} 次, 
-                        消息名称: {failed.Message.ToString()}");//do anything
-                };//失败超出次数后的回调
+            #region Cap
+            //string conn = this.Configuration.GetConnectionString("UserServiceConnection");
+            //string rabbitMQConn = this.Configuration.GetConnectionString("RabbitMQ");
+            //services.AddCap(x =>
+            //{
+            //    x.UseSqlServer(conn);//使用SQLServer的容器---数据库连接
+            //    x.UseRabbitMQ(rabbitMQConn);//使用RabbitMQ---连接地址
+            //    x.FailedRetryCount = 10;//重试次数
+            //    x.FailedRetryInterval = 60;//重试的间隔频率
+            //    x.FailedThresholdCallback = failed =>
+            //    {
+            //        var logger = failed.ServiceProvider.GetService<ILogger<Startup>>();
+            //        logger.LogError($@"MessageType {failed.MessageType} 失败了， 重试了 {x.FailedRetryCount} 次, 
+            //            消息名称: {failed.Message.ToString()}");//do anything
+            //    };//失败超出次数后的回调
 
-                //最高10次---万一真的失败10次---回调发通知---人工恢复数据让业务能通过---然后修改数据库的retries
+            //    //最高10次---万一真的失败10次---回调发通知---人工恢复数据让业务能通过---然后修改数据库的retries
 
-                x.TopicNamePrefix = "ZhaoxiDistributedTransaction";
+            //    x.TopicNamePrefix = "ZhaoxiDistributedTransaction";
 
-                #region 注册Consul可视化
-                x.UseDashboard();
-                DiscoveryOptions discoveryOptions = new DiscoveryOptions();
-                this.Configuration.Bind(discoveryOptions);
-                x.UseDiscovery(d =>
-                {
-                    d.DiscoveryServerHostName = discoveryOptions.DiscoveryServerHostName;
-                    d.DiscoveryServerPort = discoveryOptions.DiscoveryServerPort;
-                    d.CurrentNodeHostName = discoveryOptions.CurrentNodeHostName;
-                    d.CurrentNodePort = discoveryOptions.CurrentNodePort;
-                    d.NodeId = discoveryOptions.NodeId;
-                    d.NodeName = discoveryOptions.NodeName;
-                    d.MatchPath = discoveryOptions.MatchPath;
-                });
-                #endregion
-            });
+            //    #region 注册Consul可视化
+            //    x.UseDashboard();
+            //    DiscoveryOptions discoveryOptions = new DiscoveryOptions();
+            //    this.Configuration.Bind(discoveryOptions);
+            //    x.UseDiscovery(d =>
+            //    {
+            //        d.DiscoveryServerHostName = discoveryOptions.DiscoveryServerHostName;
+            //        d.DiscoveryServerPort = discoveryOptions.DiscoveryServerPort;
+            //        d.CurrentNodeHostName = discoveryOptions.CurrentNodeHostName;
+            //        d.CurrentNodePort = discoveryOptions.CurrentNodePort;
+            //        d.NodeId = discoveryOptions.NodeId;
+            //        d.NodeName = discoveryOptions.NodeName;
+            //        d.MatchPath = discoveryOptions.MatchPath;
+            //    });
+            //    #endregion
+            //});
+            #endregion
 
             #region EFCore
-            services.AddDbContext<CommonServiceDbContext>(options =>
-            {
-                options.UseSqlServer(conn);
-            });
+            //services.AddDbContext<CommonServiceDbContext>(options =>
+            //{
+            //    options.UseSqlServer(conn);
+            //});
             #endregion
 
 
